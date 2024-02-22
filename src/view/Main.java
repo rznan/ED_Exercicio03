5c0374afb82d14c15f35d147987c37d5dbdc216b
Renan <109163037+rznan@users.noreply.github.com>
2024-02-22 12:48:46 -0300

    Adicionar o enunciado

--- src/view/Main.java
+++ src/view/Main.java
@@ -2,6 +2,14 @@ package view;
 
 import controller.FatorialController;
 
+
+//    3. Crie uma função recursiva que exiba o resultado do fatorial de um número (Pela limitação da
+//    recursividade, o número de entrada deverá ser baixo para não dar estouro(limite de entrada = 12)):
+//    O código deve trazer como comentários:
+//    A condição de parada
+//    Como escrever a função para o termo n em função do termo anterior
+
+
 public class Main {
     public static void main(String[] args) {
         int value = 12;

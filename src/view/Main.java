ac8f3d5d42a2ab0050547319df2b743165d9a276
Renan <109163037+rznan@users.noreply.github.com>
2024-02-26 14:13:06 -0300

    Update Main.java

--- src/view/Main.java
+++ src/view/Main.java
@@ -2,19 +2,16 @@ package view;
 
 import controller.FatorialController;
 
-
-//    3. Crie uma função recursiva que exiba o resultado do fatorial de um número (Pela limitação da
-//    recursividade, o número de entrada deverá ser baixo para não dar estouro(limite de entrada = 12)):
-//    O código deve trazer como comentários:
-//    A condição de parada
-//    Como escrever a função para o termo n em função do termo anterior
-
-
 public class Main {
     public static void main(String[] args) {
         int value = 12;
-        var fatController = new FatorialController();
 
-        System.out.println(fatController.calculate(value));
+        if(value <= 12) {
+            var fatController = new FatorialController();
+
+            System.out.println(fatController.calculate(value));
+        }
+        else
+            System.out.println("Limite de entrada = 12");
     }
 }
